cc37c072c034194d127d6c71f1b96cdcbd85dc5e
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-09-16 11:34:55 +0100

    Implement hero area

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
-import About from "./container/About/About"
+import About from "./container/About/About";
+import Header from "./container/Header/Header";
 
 function App() {
 	return (
 		<>
 			<div className="app">
 				<NavBar />
-        <About />
+				<Header />
 			</div>
 		</>
 	);
