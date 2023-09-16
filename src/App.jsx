e90cb3209e4651cf1ea0620f7d87947efaf2d866
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-09-16 13:24:46 +0100

    Implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
-// import About from "./container/About/About";
+import About from "./container/About/About";
 import Header from "./container/Header/Header";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
 			<div className="app">
 				<NavBar />
 				<Header />
+				<About />
 			</div>
 		</>
 	);
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,61 @@
 import "./About.scss";
+// import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const abouts = [
+	{
+		title: "FrontEnd Web Developer",
+		description: "I am a good web developer",
+		imgUrl: images.about01,
+	},
+	{
+		title: "BackEnd Web Developer",
+		description: "I am a good web developer",
+		imgUrl: images.about02,
+	},
+	{
+		title: "UI/UX",
+		description: "I am a good web developer",
+		imgUrl: images.about03,
+	},
+	{
+		title: "Web Animations",
+		description: "I am a good web developer",
+		imgUrl: images.about04,
+	},
+];
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<>
+			<h2 className="head-text">
+				I Know That{" "}
+				<span>
+					Good Design <br /> means
+				</span>{" "}
+				<span>Good Business</span>
+			</h2>
+
+			<section className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: "tween" }}
+						className="app__profile-item"
+						key={about.title + index}>
+						<img src={about.imgUrl} alt={about.title} />
+						<h2 className="bold-text" style={{ marginTop: "20px" }}>
+							{about.title}
+						</h2>
+						<p className="p-text" style={{ marginTop: "10px" }}>
+							{about.description}
+						</p>
+					</motion.div>
+				))}
+			</section>
+		</>
+	);
 };
 export default About;
