a16e3c7851b4a12a6038d61c50dbd34fa8394f8d
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-09-17 14:36:00 +0100

    Implement Content management system

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,32 +1,41 @@
 import "./About.scss";
-// import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+import { useEffect, useState } from "react";
+import { urlFor, client } from "../../services/client";
 
-const abouts = [
-	{
-		title: "FrontEnd Development",
-		description: "I am a good web developer",
-		imgUrl: images.about01,
-	},
-	{
-		title: "BackEnd Development",
-		description: "I am a good web developer",
-		imgUrl: images.about02,
-	},
-	{
-		title: "UI/UX",
-		description: "I am a good web developer",
-		imgUrl: images.about03,
-	},
-	{
-		title: "Web Animations",
-		description: "I am a good web developer",
-		imgUrl: images.about04,
-	},
-];
+// const abouts = [
+// 	{
+// 		title: "FrontEnd Development",
+// 		description: "I am a good web developer",
+// 		imgUrl: images.about01,
+// 	},
+// 	{
+// 		title: "BackEnd Development",
+// 		description: "I am a good web developer",
+// 		imgUrl: images.about02,
+// 	},
+// 	{
+// 		title: "UI/UX",
+// 		description: "I am a good web developer",
+// 		imgUrl: images.about03,
+// 	},
+// 	{
+// 		title: "Web Animations",
+// 		description: "I am a good web developer",
+// 		imgUrl: images.about04,
+// 	},
+// ];
 
 const About = () => {
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+
+		client.fetch(query).then((data) => setAbouts(data));
+	}, []);
+
 	return (
 		<>
 			<h2 className="head-text">
@@ -45,7 +54,7 @@ const About = () => {
 						transition={{ duration: 0.5, type: "tween" }}
 						className="app__profile-item"
 						key={about.title + index}>
-						<img src={about.imgUrl} alt={about.title} />
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: "20px" }}>
 							{about.title}
 						</h2>
